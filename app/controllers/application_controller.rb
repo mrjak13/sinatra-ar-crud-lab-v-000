cba0b96c4deb2e7ab87b7c34bd94ce38f6e4a752
Justin Kenney <justinkenney13@gmail.com>
2018-11-13 14:28:10 +0000

    controller edit adn delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,4 +30,17 @@ class ApplicationController < Sinatra::Base
     @post = Post.find(params[:id])
     erb :show
   end
+
+  get 'posts/:id/edit' do
+    erb :edit
+  end
+
+  patch 'posts/:id' do
+
+  end
+
+  delete 'posts/:id/delete' do
+    erb :delete
+  end
+
 end
